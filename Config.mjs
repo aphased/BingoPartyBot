67005f0b298bbb5e23368bacffb5f26fcd0c4ed8
Conutik <60240193+Conutik@users.noreply.github.com>
2024-09-07 22:44:36 +0300

    s

--- Config.mjs
+++ Config.mjs
@@ -1,6 +1,6 @@
 export default {
     webhook: {
-        url: "https://discord.com/api/webhooks/1280594761740451912/M0aZO3CqZdfYuLjIPrr7XG5O0UZGo5w--Anap-Oyyjgc0nP5512wZ2Teaj2fg5RFMdRA",
+        url: "https://discord.com/api/webhooks/1280594761740451912/M0aZO3CqZNoYouDontgetThisdfYuLjIPrr7XG5O0UZG",
         name: "Bingohook",
         avatarUrl: ""
     },
