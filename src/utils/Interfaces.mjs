d35f0931f4b45edfcb949e3c5bd1f75270e599ac
aphased <152172867+aphased@users.noreply.github.com>
2024-09-14 13:54:21 +0200

    Adapt permission levels
    … to reflect ranks at the BingoBrewers' Discord server.

--- src/utils/Interfaces.mjs
+++ src/utils/Interfaces.mjs
@@ -7,7 +7,7 @@ export class DebugOptions {
     printFirst = false,
     printLast = false,
     printRank = null,
-    printUser = null
+    printUser = null,
   } = {}) {
     this.printAll = printAll;
     this.printLength = printLength;
@@ -22,23 +22,23 @@ export const MessageType = Object.freeze({
   Whisper: 0,
   PartyInvite: 1,
   PartyMessage: 2,
-  Other: 3
+  Other: 3,
 });
 
 /*
  *
  * When adding a new permission, make sure you place it in the number that corresponds to the correct weight.
  * A higher weight means a higher permission level.
- * 
-*/
+ *
+ */
 export const Permissions = Object.freeze({
   ExSplasher: 0,
   HoB: 1,
   Famous: 1,
   Splasher: 2,
   Trusted: 2,
-  Admin: 3,
   Staff: 3,
-  Owner: 4,
-  BotAccount: 4
-})
+  Admin: 4,
+  Owner: 5,
+  BotAccount: 5,
+});
