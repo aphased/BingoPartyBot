ece9ce71de409d01ddd1aaac3540e5ed5237f896
aphased <152172867+aphased@users.noreply.github.com>
2024-09-14 13:34:15 +0200

    Move `reply()` from utils to bot class
    architecture/project design pro™️

--- src/mineflayer/Bot.mjs
+++ src/mineflayer/Bot.mjs
@@ -5,6 +5,9 @@ import loadPartyCommands from "./handlers/PartyCommandHandler.mjs";
 class Bot {
   constructor() {
     this.config = config.default;
+    /**
+     * The Mineflayer bot on Hypixel.
+     */
     this.bot = mineflayer.createBot({
       host: "mc.hypixel.net",
       username: this.config.mineflayerInfo.email,
@@ -28,6 +31,30 @@ class Bot {
 
   chat(message) {
     this.bot.chat(message);
+  }
+
+  /**
+   * Use this function anytime you want to send a message via Hypixel's `/r`.
+   * Currently, the recipient is unused anyway, so if they're unknown, pass an
+   * empty string (`""`).
+   * The `reply` function will add on a randomizing part at the end of the
+   * message string in order to enable repeated output/sending of the same
+   * direct message (womp womp Hypixel).
+   *
+   * Due to direct message privacy settings, this implementation currently
+   * exclusively uses the `/reply` mechanism in favor of `/msg` (=`/whisper`).
+   * Advantage is being able to reply to anybody, at the cost of potentially
+   * answering the wrong message to an incorrect recipient with quick
+   * back-to-back commands/messages received.
+   
+   * Thus, this use may change in the future, at which point including the
+   * recipient's info (i.e. IGN) will be necessary.
+   * @param {String} recipient  IGN to send a message to (currently not needed,
+   * but try to supply this value).
+   * @param {String} message  Message to send.
+   */
+  reply(recipient, message) {
+    this.chat(`/r ${this.addRandomString(message)}`);
   }
 
   async reloadPartyCommands() {
