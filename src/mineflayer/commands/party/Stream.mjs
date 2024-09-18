80487e558d7274b4097545519a79edd1c3023799
aphased <152172867+aphased@users.noreply.github.com>
2024-09-18 10:57:58 +0200

    Formatting + appropriate function usage
    in `!p stream` command

--- src/mineflayer/commands/party/Stream.mjs
+++ src/mineflayer/commands/party/Stream.mjs
@@ -1,30 +1,29 @@
 import { Permissions } from "../../../utils/Interfaces.mjs";
 
 export default {
-    name: ["stream", "public", "open"], // This command will be triggered by either command1 or command2
-    ignore: false, // Whether to ignore this file or not
-    description: "Kick Command", // Description of the command
-    permission: Permissions.Trusted, // Permission level required to execute this command
-    /**
-     * 
-     * @param {import("../../Bot.mjs").default} bot 
-     * @param {String} sender 
-     * @param {Array<String>} args 
-     */
-    execute: async function (bot, sender, args) {
-        let amt = parseInt(args[0]) || 100;
-        bot.bot.chat(`/pc Party size was set to ${amt} by ${sender}`);
-        setTimeout(() => {
-            bot.bot.chat("/stream open " + amt);
-            bot.webhook.send(
-                {
-                    username: bot.config.webhook.name,
-                },
-                {
-                    content: `Party was set to public (${amt}). Command executed by ${sender}`,
-                }
-            );
-        }, 1000)
-    }
-  }
-  
+  name: ["stream", "public", "open"], // This command will be triggered by either command1 or command2
+  ignore: false, // Whether to ignore this file or not
+  description: "Kick Command", // Description of the command
+  permission: Permissions.Trusted, // Permission level required to execute this command
+  /**
+   *
+   * @param {import("../../Bot.mjs").default} bot
+   * @param {String} sender
+   * @param {Array<String>} args
+   */
+  execute: async function (bot, sender, args) {
+    let amount = parseInt(args[0]) || 100;
+    bot.chat(`/pc Party size was set to ${amount} by ${sender}`);
+    setTimeout(() => {
+      bot.chat("/stream open " + amount);
+      bot.webhook.send(
+        {
+          username: bot.config.webhook.name,
+        },
+        {
+          content: `Party was set to public (${amount}). Command executed by ${sender}`,
+        },
+      );
+    }, 1000);
+  },
+};
