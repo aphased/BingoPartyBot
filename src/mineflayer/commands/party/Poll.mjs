ada8e95431fd0c8c062794be7ad27d46824d0c73
aphased <152172867+aphased@users.noreply.github.com>
2024-09-21 11:39:30 +0200

    Add poll validity checks
    (same as they were present in the pre-rewrite version)

--- src/mineflayer/commands/party/Poll.mjs
+++ src/mineflayer/commands/party/Poll.mjs
@@ -13,41 +13,39 @@ export default {
    */
   execute: async function (bot, sender, args) {
     let poll = args.join(" ");
-    // TODO: add checks on poll validity, and `bot.reply()` with the reason if
-    // it fails those. See pre-rewrite handling of this for reference below:
 
-    // // Some basic checks on poll message validity. If a message contains less
-    // // than exactly 2 or more than 5 slashes ("/"), Hypixel is likely going to
-    // // reject it (minimum two poll options, max. 5), thus:
-    // let slashCharCount = 0;
-    // for (let i = 0; i < pollMessage.length; i++) {
-    //   if (pollMessage[i] === "/") {
-    //     slashCharCount++;
-    //   }
-    // }
-    // logDebug("Party poll slashCharCount: '" + slashCharCount + "'");
+    // Some basic checks on poll message validity. If a message contains less
+    // than exactly 2 or more than 5 slashes ("/"), Hypixel is likely going to
+    // reject it (minimum two poll options, max. 5), thus:
+    let slashCharCount = 0;
+    for (let i = 0; i < poll.length; i++) {
+      if (poll[i] === "/") {
+        slashCharCount++;
+      }
+    }
 
-    // // Another check – Hypixel: "Each answer can only be 20 characters long."
-    // // (questions part can be longer, so we shift() the array once to discard it from this test)
-    // let portions = pollMessage.split("/");
-    // portions.shift();
-    // let answerLengthsValid = portions.every((portion) => {
-    //   return portion.length <= 20;
-    // });
+    // Another check – Hypixel: "Each answer can only be 20 characters long."
+    // (questions part can be longer, so we shift() the array once to
+    // discard it from this test)
+    let portions = poll.split("/");
+    portions.shift();
+    let answerLengthsValid = portions.every((portion) => {
+      return portion.length <= 20;
+    });
 
-    // // TODO: this is the boolean to update in case of adding new checks on
-    // // poll message validity, if it is to be made more sophisticated:
-    // const isValidPoll =
-    //   slashCharCount >= 2 && slashCharCount <= 5 && answerLengthsValid;
+    // TODO: this is the boolean to update in case of adding new checks on poll
+    // message validity, if it is to be made more sophisticated (e.g. maximum
+    // overall poll message length when sending from Discord in the future?):
+    const isValidPoll =
+      slashCharCount >= 2 && slashCharCount <= 5 && answerLengthsValid;
 
-    // if (!isValidPoll) {
-    //   outputCommand(
-    //     `r Hi ${rankRemovedSenderName}, what you sent wasn't a valid poll!`,
-    //   );
-    // } else {
-    //   outputCommand(`p poll From ${formattedSenderName}: ${pollMessage}`);
-    // }
-
-    bot.chat(`/p poll ${sender}: ${poll}`);
+    if (!isValidPoll) {
+      bot.reply(
+        sender.username,
+        `Hi ${sender.username}, what you sent wasn't a valid poll!`,
+      );
+    } else {
+      bot.chat(`/p poll From ${sender}: ${poll}`);
+    }
   },
 };
