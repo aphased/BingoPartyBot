94a3d4a4f75e6d26e8a55ac4a40d01961b15e44f
BossFlea <flo.boss@outlook.com>
2024-10-14 18:16:22 +0200

    Added new command property `alwaysEnabled`

--- src/mineflayer/commands/EXAMPLECOMMAND.mjs
+++ src/mineflayer/commands/EXAMPLECOMMAND.mjs
@@ -7,6 +7,7 @@ export default {
   permission: Permissions.Admin, // Permission level required to execute this command
   customPrefix: "", // Only use this if you want to use a custom prefix for this command, otherwise leave it empty and it'll use the default prefix
   isPartyChatCommand: false, // Set to true if the command should be used in party chat, also include the command's prefix (if applicable) in the name (e.g. '!guide')
+  alwaysEnabled: false, // Set to true for commands that should never be disabled (e.g. "!p disable")
   /**
    *
    * @param {import("../../Bot.mjs").default} bot
