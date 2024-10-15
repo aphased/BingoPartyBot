a897b151dc5197435ec6db248524f37c84ecd5cc
aphased <152172867+aphased@users.noreply.github.com>
2024-10-15 23:42:37 +0200

    Add example Discord command structure to Docgen
    (TODO)

--- src/discord/components/Docgen.mjs
+++ src/discord/components/Docgen.mjs
@@ -55,3 +55,25 @@ function transformMarkdownToDiscord(markdown) {
 
   return discordMessage.trim();
 }
+
+/* From src/discord/components/commands/ExampleCommand.mjs: */
+// import { ApplicationCommandOptionType, Client } from "discord.js";
+
+// export default {
+//   name: "example",
+//   options: [
+//     {
+//       name: "example",
+//       description: "example",
+//       type: ApplicationCommandOptionType.String,
+//     },
+//   ],
+//   ignore: true,
+//   description: "example",
+//   /**
+//    *
+//    * @param {Client} bot
+//    * @param {import("discord.js").Interaction} interaction
+//    */
+//   execute: async function (bot, interaction) {},
+// };
