c8ffcf0f006524ab7124200a505eb73b5c88aef2
BossFlea <flo.boss@outlook.com>
2024-10-19 11:14:10 +0200

    Updated reply messages for `!p removeuser`

--- src/mineflayer/commands/admin/RemoveUser.mjs
+++ src/mineflayer/commands/admin/RemoveUser.mjs
@@ -14,11 +14,16 @@ export default {
   execute: async function (bot, sender, args) {
     let user = args[0];
     let only = args[1]?.toLowerCase?.() === "only";
+    if (!user)
+      return bot.reply(
+        sender,
+        'Please supply a username to remove! You can choose to keep the person\'s other accounts by appending "only".',
+      );
     if (!bot.utils.getUserObject({ name: user }))
       return bot.reply(sender, `User ${user} not found in database!`);
     bot.utils.removeUser({ name: user, onlyThis: only });
     if (only)
       return bot.reply(sender, `Removed account ${user}, but kept other alts.`);
-    bot.reply(sender, `Removed user ${user}.`);
+    bot.reply(sender, `Removed user ${user} and all their other accounts.`);
   },
 };
