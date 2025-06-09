827bbba5fdc477b6ed3a7b5819d0ad86d216e14f
unterrunden <133593594+unterrunden@users.noreply.github.com>
2025-06-09 23:31:49 +0200

    Update Ban.mjs
    - reworked order of banning: /lobby -> /block add -> /p kick (switching around last 2)
    - added a statement that checks if the inputted user exists (previously only found in Unban.mjs)
    - extended "you cannot ban someone with higher a permission level" in that it includes your and the target player's permission and level
    - upon banning, sets permission level of affected user to 0 and logs that to the console

--- src/mineflayer/commands/party/Ban.mjs
+++ src/mineflayer/commands/party/Ban.mjs
@@ -18,29 +18,66 @@ export default {
    */
   execute: async function (bot, sender, args) {
     let player = args[0];
-    if (!player)
+    if (!player) {
+      return bot.reply(sender, `Invalid usage! Use: ${this.usage}`, VerbosityLevel.Reduced);
+    }
+    const playerExists = await bot.utils.usernameExists(player);
+    if (playerExists === false) {
+      return bot.reply(sender, `Player ${player} not found`, VerbosityLevel.Reduced);
+    }
+    const reason = args.slice(1).join(" ") || "No reason given.";
+      
+    if (!bot.utils.isHigherRanked(sender.username, player)) {
+      const senderPermsRank = bot.utils.getPermissionsByUser({ name: sender.username });
+      const playerPermsRank = bot.utils.getPermissionsByUser({ name: player });
+      const senderPerms = Object.keys(Permissions).find(
+        (perm) => Permissions[perm] === senderPermsRank,
+      );
+      const playerPerms = Object.keys(Permissions).find(
+        (perm) => Permissions[perm] === playerPermsRank,
+      );
       return bot.reply(
         sender,
-        `Invalid usage! Use: ${this.usage}`,
+        `You cannot ban a player of a higher permission level than yourself (your rank: ${senderPerms} (level: ${senderPermsRank}), their rank: ${playerPerms} (level: ${playerPermsRank}).`,
         VerbosityLevel.Reduced,
       );
-    let reason = args.slice(1).join(" ") || "No reason given.";
-    if (!bot.utils.isHigherRanked(sender.username, player)) {
-      return;
     }
-    bot.chat(
-      `/pc ${player} was removed from the party and blocked from rejoining by ${sender.preferredName}.`,
-    );
+    
+    bot.reply(sender, `Trying to ban ${player}...`, VerbosityLevel.Reduced);
+    
     await bot.utils.delay(bot.utils.minMsgDelay);
     bot.chat(`/lobby`);
     await bot.utils.delay(bot.utils.minMsgDelay);
+    bot.chat(`/block add ${player}`);
+    await bot.utils.delay(bot.utils.minMsgDelay);
     bot.chat(`/p kick ${player}`);
     await bot.utils.delay(bot.utils.minMsgDelay);
-    bot.chat(`/block add ${player}`);
+    bot.reply(sender, `Banned ${player}.`, VerbosityLevel.Reduced);
+
+    await bot.utils.delay(bot.utils.minMsgDelay);
+    bot.chat(
+      `/pc ${player} was removed from the party and blocked from rejoining by ${sender.preferredName}.`,
+    );
+
     bot.utils.webhookLogger.addMessage(
       `\`${player}\` was banned from the party by \`${sender.preferredName}\`. Reason: \`${reason}\``,
       WebhookMessageType.ActionLog,
       true,
     );
+
+    if (bot.utils.getUserObject({ name: player })) {
+      bot.utils.setPermissionRank({
+        name: player,
+        newPermissionRank: 0,
+      });
+      const newPlayerPerms = Object.keys(Permissions).find(
+        (perm) => Permissions[perm] === newPermissionRank,
+      );
+      bot.utils.webhookLogger.addMessage(
+        `After banning, \`${player}\`'s permission rank was updated to \`${newPlayerPerms}\` (level: \`${newPermissionRank}\`) by \`${sender.username}\`.`,
+        WebhookMessageType.ActionLog,
+        true,
+      );
+    }
   },
 };
